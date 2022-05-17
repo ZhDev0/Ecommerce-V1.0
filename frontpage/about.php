e22d45bb34c78be580734887178637766697ab31
ZhDev0 <105553499+ZhDev0@users.noreply.github.com>
2022-05-17 17:43:57 +0100

    First Commit
    First Commit

---
+++ frontpage/about.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="./frontimages/Screenshot 2022-05-06 203524.png"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZhDevShop</title>
+    <link rel="stylesheet" href="./style.css">
+    <style>
+        footer {
+            height: 60px;
+        }
+        footer .text-center {
+            text-align: center;
+            background: white;
+            position: absolute;
+            bottom: -1100px;
+            width: 100%;
+            height: 5vh;
+            background-color: transparent;
+            color: #fff;
+            font-size: 20px;
+            padding-top: 25px;
+            padding-bottom: 50px;
+            letter-spacing: 0.15rem;
+        }
+        @media (max-width: 768px) {
+            footer .text-center {
+                bottom: -2550px;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <nav class="navbar">
+                <a href="./index.php" class="nav-branding">ZhDevShop</a>
+                <ul class="nav-menu" style="padding: 32px 0;">
+                    <li style="padding: 8px 16px;" class="nav-item"><a href="./index.php" class="nav-link">Home</a></li>
+                    <li style="padding: 8px 16px;" class="nav-item"><a href="#" class="nav-link">About</a></li>
+                    <li style="padding: 8px 16px;" class="nav-item"><a href="./contacts.php" class="nav-link">Contacts</a></li>
+                    <li style="padding: 8px 16px;" class="nav-item"><a href="../admin/products1.php" class="nav-link">Products</a></li>
+                    <li class="nav-item"><button style="border-radius: 15px;padding: 8px 16px;"><a href="../register.php" class="nav-link">Register</a></button></li>
+                    <li class="nav-item"><button style="border-radius: 15px;padding: 8px 16px;"><a href="../login.php" class="nav-link">Sign Up</a></button></li>
+                </ul>
+                <div class="hamburger">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+            </nav>
+        </header>
+    </div>
+        <div class="about-section">
+            <h1>ZhDevShop</h1>
+            <img src="./frontimages/Screenshot 2022-05-06 203524.png">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor amet perferendis nesciunt id quae odio assumenda maxime suscipit mollitia obcaecati. Quos magnam aliquam et eum officiis delectus in beatae blanditiis sapiente! Recusandae, mollitia quas nostrum in quam vero commodi blanditiis reprehenderit totam illum, officiis molestias voluptatem. Exercitationem, quaerat? Eveniet incidunt culpa qui saepe eligendi, voluptate omnis necessitatibus deserunt fugit optio debitis alias ex eum maiores rerum iusto ab assumenda vel placeat asperiores ut eius? Quisquam tenetur architecto ex corrupti. Repellendus, praesentium cupiditate. Sunt hic aspernatur saepe accusantium quas maiores voluptate officia magnam quidem, ab cum libero, perspiciatis natus nemo voluptatem debitis eius ad. Provident consequatur aperiam facere autem et, aut doloremque voluptate tempore, quidem blanditiis obcaecati quia, minus eos suscipit cumque unde delectus iste eum recusandae corporis ratione fuga enim amet? Neque quod dolorum alias nam. Nostrum aperiam enim delectus deserunt consectetur non est? Excepturi suscipit consequatur, unde rerum amet aliquid autem voluptate, eligendi sunt minima quisquam? Repellendus nihil aut neque tempora. Dolores vitae delectus repudiandae non? Eaque molestias totam culpa ea doloribus fuga harum, repellat esse est aliquam accusamus eligendi impedit quos dolore amet? Placeat sed officiis eum nihil veritatis, adipisci, esse fuga sunt aperiam rerum neque laudantium dicta mollitia labore ad eligendi. Illum optio accusantium necessitatibus illo odio eius beatae nam tempora magni dolore, quisquam asperiores, delectus commodi? Neque impedit ea aut nemo vel aspernatur velit? Porro eum dolorem fuga voluptatum ipsa quam corporis accusamus cumque, tempora, ducimus quae odit deserunt labore esse, quasi facere. Dolores, quasi! Atque, tempora recusandae. Autem earum illo necessitatibus commodi officiis dolore enim! Vel, beatae excepturi. Natus fugit alias in enim, rem est odit, perspiciatis temporibus quos fugiat veritatis tempora quod doloremque magni totam accusamus debitis eaque, vel at animi asperiores recusandae maiores quasi. Eveniet dignissimos omnis veniam, vero commodi, blanditiis est totam molestias, itaque quasi consequatur magni eos libero debitis doloribus veritatis. Tempora nostrum obcaecati libero quis! Voluptates labore dolores soluta repellat sequi! Quo officiis, non vitae ut iste sint voluptate, commodi error omnis ipsam ratione debitis ad reiciendis velit aspernatur officia odio veniam, tempora unde doloribus aliquam. Animi harum numquam unde laborum pariatur recusandae laudantium nesciunt vel, eos quo quas enim perspiciatis quisquam debitis quaerat voluptatum molestias a consequuntur aliquam. Aspernatur, et! Doloremque dicta asperiores officia labore quia molestias consequuntur dolor aperiam soluta minus? Ducimus hic necessitatibus sit, est iusto veritatis aliquid commodi laboriosam fugiat debitis dolor recusandae at ipsa, blanditiis laborum delectus? Odio eaque, voluptatum saepe facilis, aperiam, veritatis fuga dolorem repellat autem quam sint porro officia eveniet? Velit numquam ipsam iure voluptatum quo distinctio, asperiores blanditiis maxime. Delectus quo, libero distinctio deserunt similique in cum unde architecto laudantium neque magni facilis repellat, voluptates recusandae nostrum quae. Nesciunt qui ipsam quaerat, officia incidunt culpa, nam distinctio, et consequuntur harum explicabo impedit. Assumenda, iusto doloremque iste minima, eveniet voluptates ipsum, nam inventore optio similique rem cumque quam totam culpa porro! Reprehenderit atque nulla minima maiores perspiciatis error! Eaque porro magnam atque illum eius maiores id deleniti autem tenetur unde, temporibus architecto totam hic repudiandae excepturi quae! Nobis accusantium excepturi repudiandae ipsa. Tempore quis dolor mollitia eligendi ipsum, consequatur nisi dolorem enim. Dolorum, eaque! Nihil et accusantium obcaecati ipsa magni, excepturi possimus ex commodi sapiente voluptates error doloribus, quia dignissimos laborum tempore minima esse illum, fugit hic deleniti nulla harum doloremque? Eaque libero quisquam voluptatem odit facere quos nesciunt aperiam velit! Laboriosam blanditiis totam dignissimos voluptatum quidem, minima doloribus saepe doloremque modi cum consequatur. Delectus dolorum perspiciatis nihil facere commodi ratione! Dolorem iste, reprehenderit distinctio maiores deserunt excepturi odit, quidem voluptate numquam mollitia architecto repellendus dolore quasi sapiente debitis quibusdam repudiandae perferendis iusto perspiciatis temporibus inventore. Soluta nihil odio vitae commodi atque incidunt nemo magnam architecto aut omnis voluptatem blanditiis inventore dignissimos, reprehenderit unde culpa ducimus eum aperiam deleniti quaerat ratione? Eveniet facilis aut cum! Ipsam debitis libero velit nemo earum officia, similique facilis eaque, voluptas provident laudantium cupiditate illo eos quod amet expedita quidem temporibus et optio, veniam architecto nesciunt repellendus itaque quas. Mollitia cupiditate architecto libero! Ipsa itaque qui, fuga ex soluta vitae eligendi! Sit odit sint excepturi natus corrupti maiores eligendi amet at recusandae dolores nesciunt cupiditate atque error saepe molestias ipsa expedita, suscipit est dolor nam illo a quia sequi adipisci. Velit illum fuga id sit quasi? Aliquid eum, quidem totam ex dicta recusandae non voluptas reprehenderit. Dignissimos, ipsam deleniti labore id quod, alias itaque dicta non voluptate dolorum nam. Repellat, cumque molestias? Magni facilis voluptatem ipsum excepturi laboriosam modi soluta saepe ad ipsam possimus repellendus perferendis illum necessitatibus aliquam animi exercitationem nulla natus, aut debitis delectus ab architecto officia fugit! Mollitia cupiditate ducimus, quam neque molestiae laboriosam corrupti voluptatum tenetur eveniet soluta enim atque commodi. Id aperiam et commodi? Fugit rem velit dolorem error nisi officia sint autem saepe consequatur incidunt fugiat nulla consectetur excepturi vero eaque natus, veritatis quas deleniti temporibus quia minus cupiditate culpa? Temporibus nihil nulla dolorem iusto animi debitis fuga aliquid? Quidem consectetur porro, quibusdam, cum consequuntur placeat aliquid dolorum illo officiis expedita voluptatem inventore est tenetur unde qui, temporibus veritatis aspernatur odio earum optio ex molestiae dolor modi! Dolorum, vitae. Iste est ratione distinctio et, labore, non nostrum autem mollitia voluptate voluptatem minima nihil doloribus quidem natus impedit. Temporibus voluptatibus, ratione molestiae aut explicabo neque dolores deleniti ipsa quibusdam amet nemo fuga perspiciatis nisi modi quidem deserunt a! Cum magni ea aliquid ab enim perspiciatis eligendi ut aut quam harum neque magnam, quasi non, nulla soluta laboriosam pariatur culpa. Ratione recusandae magnam iste aperiam exercitationem, animi incidunt nesciunt et consequatur. Accusantium ex corrupti, deserunt enim dicta a hic dolore cum aut minima facere tenetur magnam soluta ab accusamus perferendis explicabo officia recusandae nostrum id voluptate itaque fugiat! Explicabo saepe ea quam dicta harum, error, doloribus corrupti beatae et sit pariatur totam aut, inventore non. Esse laudantium exercitationem numquam illum rem doloremque dolore ea? Obcaecati assumenda, repellendus cumque saepe debitis ab modi accusamus dolor officiis, dolores vero, illo similique provident deserunt unde optio maiores soluta reiciendis id. Est sed facilis ipsam sequi quaerat!</p>
+        </div>
+
+
+
+    <footer>
+        <p class="text-center">Copyright &copy;2022 ZhDevShop</p>
+    </footer>
+    <script src="script.js"></script>
+    <script src="./typed.js"></script>
+    <script>
+        var typed = new Typed('.animate', {
+            strings: [
+                "<span style='color: yellow;'><b>Latest Iphones - Mac</b></span>",
+                "<span style='color: orange;'><b>Cleaning Tools</b></span>",
+                "<span style='color: lightblue;'><b>Health and Beauty Products</b></span>",
+                "<span style='color: red;'><b>Accessories</b></span>",
+                "<span style='color: lightgreen;'><b>And More ...</b></span>",
+            ],
+            typeSpeed: 20,
+            backSpeed: 100,
+            backDelay: 700,
+            loop: true
+        });
+    </script>
+</body>
+
+</html>
